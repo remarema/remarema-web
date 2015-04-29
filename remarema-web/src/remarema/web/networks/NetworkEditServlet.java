00ec988d8299e898af8eabfc5164118358ef1f8f
rebeccavl1 <rebeccavanlangelaan@yahoo.com>
2015-04-29 17:21:11 +0200

    some changes

--- remarema-web/src/remarema/web/networks/NetworkEditServlet.java
+++ remarema-web/src/remarema/web/networks/NetworkEditServlet.java
@@ -26,81 +26,87 @@ import remarema.web.util.CookieHelper;
 @WebServlet("/network_edit")
 public class NetworkEditServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Inject
 	private NetworkServiceBean networkService;
-	
+
 	@Inject
 	private NodeServiceBean nodeService;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public NetworkEditServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		if(CookieHelper.checkCookie(request, 2)){
-			request.getRequestDispatcher("/error.jsp").forward(request, response);
+	public NetworkEditServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		if (CookieHelper.checkCookie(request, 2)) {
+			request.getRequestDispatcher("/error.jsp").forward(request,
+					response);
 		}
-		
+
 		Integer networkID = new Integer(request.getParameter("id"));
-		
+
 		NetworkDetail networkDetail = new NetworkDetail();
 		networkDetail.setNetworkID(networkID);
-		NetworkDetail nwd = networkService.getNetworkDetailForNetworkID(networkDetail);
-		
+		NetworkDetail nwd = networkService
+				.getNetworkDetailForNetworkID(networkDetail);
+
 		request.setAttribute("id", nwd.getNetworkID());
 		request.setAttribute("name", nwd.getNetworkName());
 		request.setAttribute("parentID", nwd.getNetworkParentID());
 		request.setAttribute("parentName", nwd.getNetworkParentName());
-		
-		
-		request.getRequestDispatcher("/network_edit.jsp").forward(request, response);
+
+		request.getRequestDispatcher("/network_edit.jsp").forward(request,
+				response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		if(CookieHelper.checkCookie(request, 2)){
-			request.getRequestDispatcher("/error.jsp").forward(request, response);
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		if (CookieHelper.checkCookie(request, 2)) {
+			request.getRequestDispatcher("/error.jsp").forward(request,
+					response);
 		}
-		
+
 		Integer id = new Integer(request.getParameter("id"));
 		String name = request.getParameter("name");
 		String parentID = request.getParameter("parentID");
 		String parentName = request.getParameter("parentName");
 		String action = request.getParameter("action");
-		
-		if(action.equals("update")){
+
+		if (action.equals("update")) {
 			UpdateNetwork updateNetwork = new UpdateNetwork();
 			updateNetwork.setNetworkID(id);
 			updateNetwork.setNetworkName(name);
 			updateNetwork.setNetworkParentName(parentName);
-			
+
 			request.setAttribute("id", id);
 			request.setAttribute("name", name);
 			request.setAttribute("parentName", parentName);
-			
-			
-		
+
 			networkService.networkUpdate(updateNetwork);
-			
+
 			NetworkDetail networkDetail = new NetworkDetail();
 			networkDetail.setNetworkID(id);
-			NetworkDetail nwd = networkService.getNetworkDetailForNetworkID(networkDetail);
+			NetworkDetail nwd = networkService
+					.getNetworkDetailForNetworkID(networkDetail);
 			request.setAttribute("parentID", nwd.getNetworkParentID());
-			
+
 			request.setAttribute("message", "&Auml;nderungen erfolgreich!");
-			request.getRequestDispatcher("/network_edit.jsp").forward(request, response);
-		}
-		else if(action.equals("delete")){
+			request.getRequestDispatcher("/network_edit.jsp").forward(request,
+					response);
+		} else if (action.equals("delete")) {
 			NetworkDetail networkDetail = new NetworkDetail();
 			networkDetail.setNetworkID(id);
 			try {
@@ -109,52 +115,50 @@ public class NetworkEditServlet extends HttpServlet {
 			} catch (ChildNotEmptyException e) {
 				request.setAttribute("message", e.getMessage());
 				e.printStackTrace();
-				request.getRequestDispatcher("/network_edit.jsp").forward(request, response);
+				request.getRequestDispatcher("/network_edit.jsp").forward(
+						request, response);
 			}
-		}
-		else if(action.equals("insertChild")){
+		} else if (action.equals("insertChild")) {
 			String childName = request.getParameter("childName");
-			
+
 			CreateNetwork command = new CreateNetwork();
 			command.setNetworkName(childName);
 			command.setParentNetworkName(name);
 			networkService.execute(command);
 			request.setAttribute("message", "Child erfolgreich erstellt!");
-			
+
 			request.setAttribute("id", id);
 			request.setAttribute("name", name);
 			request.setAttribute("parentName", parentName);
 			request.setAttribute("parentID", parentID);
-			
-			request.getRequestDispatcher("/network_edit.jsp").forward(request, response);
-		}
-		else if(action.equals("insertNode")){
+
+			request.getRequestDispatcher("/network_edit.jsp").forward(request,
+					response);
+		} else if (action.equals("insertNode")) {
 			String nodeName = request.getParameter("nodeName");
 			String nodeIP = request.getParameter("nodeIP");
-			
+
 			CreateNode createNode = new CreateNode();
 			createNode.setNodeName(nodeName);
 			createNode.setNodeIP(nodeIP);
 			createNode.setNodeNetworkName(name);
-			
+
 			nodeService.execute(createNode);
 
 			request.setAttribute("message", "Client erfolgreich erstellt!");
-			
+
 			request.setAttribute("id", id);
 			request.setAttribute("name", name);
 			request.setAttribute("parentName", parentName);
 			request.setAttribute("parentID", parentID);
-			
-			request.getRequestDispatcher("/network_edit.jsp").forward(request, response);
-		}
-		else{
-			request.getRequestDispatcher("/network_edit.jsp").forward(request, response);
+
+			request.getRequestDispatcher("/network_edit.jsp").forward(request,
+					response);
+		} else {
+			request.getRequestDispatcher("/network_edit.jsp").forward(request,
+					response);
 		}
 
-		
-		
-		
 	}
 
 }
